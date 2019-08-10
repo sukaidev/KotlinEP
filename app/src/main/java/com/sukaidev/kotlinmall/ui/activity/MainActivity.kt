275a66b54e813bc8d631f6b6e94ba35d2413422c
sukaidev <suhaihong88@gmail.com>
2019-08-10 11:38:26 +0800

    初始化项目结构.

---
+++ app/src/main/java/com/sukaidev/kotlinmall/ui/activity/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.sukaidev.kotlinmall.ui.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.sukaidev.kotlinmall.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
